3e01430831f1630db401d4e7ff254170adf5037a
MSC791 <63659790+MSC791@users.noreply.github.com>
2020-05-03 16:46:39 +0200

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Apr 20 2020
-
-@authors: Julia Gruber, Max Schlicher, Caroline Steiner, Jelena Zimmermann
-"""
 def which_cantons():
     #needed for showing the list of cantons
     import pandas as pd
